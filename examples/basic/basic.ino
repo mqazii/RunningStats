4da7dc1dd1321a5c3d686c9a332723570ea28d10
mqazii <144493185+mqazii@users.noreply.github.com>
2024-01-19 10:47:03 -0800

    Update basic.ino

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -11,8 +11,8 @@ void loop() {
     // Feed the float value here
     stats.update(1.0);
 
-    Serial.println(String("The standard deviation is " + stats.mean()));
-    Serial.println(String("The means is " + stats.std()));
+    Serial.println(String("The standard deviation is " + stats.std()));
+    Serial.println(String("The means is " + stats.mean()));
 
     delay(1000);
 }
